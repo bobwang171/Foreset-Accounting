583b9b94c64e42ea0a76231cf2a8ae1e60795c23
bobwang171 <103095277+bobwang171@users.noreply.github.com>
2022-10-12 11:25:13 +0800

    使用jsx代替template

--- src/App.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    hi
-    
-  </div>
-</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'/bobwang171/Mangosteen-Accounting/dist/',
-  plugins: [vue()]
+  plugins: [vue(),
+    vueJsx({
+      transformOn: true,
+      mergeProps:true
+    // options are passed on to @vue/babel-plugin-jsx
+  })]
+
 })
