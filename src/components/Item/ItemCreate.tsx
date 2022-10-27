65d5d1fc0bfe6676dd9cd3f2d29dd27d596dbfad
bobwang171 <103095277+bobwang171@users.noreply.github.com>
2022-10-27 16:37:30 +0800

    wip: Tabs组件

--- src/components/Item/ItemCreate.tsx
+++ src/components/Item/ItemCreate.tsx
@@ -1,18 +1,26 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
 import { Icon } from '../../shared/icon';
+import { Tab, Tabs } from '../../shared/Tabs';
 import s from './ItemCreate.module.scss'
 export const ItemCreate = defineComponent({
   setup: (props, context) => {
+    const refKind = ref("支出")
     return () => (
       <MainLayout class={s.navbar}>{
         {
               title: () => "记一笔",
               icon: () => <Icon name='return' class={s.navIcon} />,
               default: () =>
-                <>
-                  aaa
-              </>
+                <Tabs selected={refKind.value} onUpdateSelected={(name:string)=>refKind.value=name}>
+                  <Tab name="支出">
+                    Icon列表1
+                  </Tab>
+                  <Tab name="收入">
+                    Icon列表2
+                  </Tab>
+                </Tabs>
+              
             }
           }</MainLayout>
     )
---
+++ src/shared/Tabs.tsx
@@ -0,0 +1,54 @@
+import { defineComponent, PropType } from 'vue';
+import s from './Tabs.module.scss'
+export const Tabs = defineComponent({
+
+  props: {
+    selected: {
+      type:String as PropType<string>
+    },
+    onUpdateSelected: {
+      type:Function as PropType<(name:string)=>void >
+    }
+  },
+
+
+  setup: (props, context) => {
+    return () => {
+      const array = context.slots.default?.()
+      if (!array) return () => null
+      for (let i = 0; i < array.length; i++) {
+        if (array[i].type !== Tab)
+          throw new Error("<Tabs> only accept <Tab> as children")
+      }
+      return <div> 
+          <ol class={s.tabs_nav}>
+          {array.map(item =>
+            <li class={item.props?.name === props.selected ? s.selected : ""}
+              onClick={()=>props.onUpdateSelected?.(item.props?.name)}
+            >
+              {item.props?.name}</li>)}
+          </ol>
+      </div>
+    }
+
+  }
+  
+})
+
+
+export const Tab = defineComponent({
+  props: {
+    name: {
+      type:String as PropType<string>
+    }
+  },
+  setup: (props, context) => {
+
+    return () => (
+      <div>{context.slots.default?.()}
+        
+      </div>
+      
+    )
+  }
+})
