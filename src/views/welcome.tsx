6eae5962855b66e602caf9afe65cc8d853dbaf38
bobwang171 <103095277+bobwang171@users.noreply.github.com>
2022-12-06 17:28:08 +0800

    修改logo

--- src/views/welcome.tsx
+++ src/views/welcome.tsx
@@ -1,15 +1,14 @@
 import { defineComponent } from "vue"
 import { RouterView } from 'vue-router';
 import s from "./Welcome.module.scss"
+import { Icon } from '../shared/icon';
 
 export const Welcome = defineComponent({
     setup: (props, context) => {
         return () => <div class={s.wrapper}>
 
             <header>
-                <svg>
-                    <use xlinkHref="#mangosteen"></use>
-                </svg>
+                <Icon name="forest" />
                 <h1>森林记账</h1>
             </header>
             <main class={s.main}><RouterView /></main>
